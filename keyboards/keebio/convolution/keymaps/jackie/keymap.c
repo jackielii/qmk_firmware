0091cdaf2d608617762d8bf9a313d01492d294ff
Jackie Li <jackie.space@gmail.com>
2025-11-01 16:28:04 +0000

    .

--- keyboards/keebio/convolution/keymaps/jackie/keymap.c
+++ keyboards/keebio/convolution/keymaps/jackie/keymap.c
@@ -43,39 +43,39 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_all(
-    KC_MPRV,        KC_MNXT, QK_GESC,    KC_1,     KC_2,     KC_3,       KC_4,          KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,         KC_GRV,  KC_MPLY,
-    JL_CUT_MAC,     KC_PAUS, KC_TAB,     KC_Q,     KC_W,     KC_E,       KC_R,          KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,         KC_DEL,
-    JL_SUP_CTL_TAB, KC_MCTL, JL_CTL_ESC, KC_A,     KC_S,     KC_D,       KC_F,          KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,          KC_PGUP,
-    KC_F7,          KC_F8,   KC_LSFT,    KC_NUBS,  KC_Z,     KC_X,       KC_C,          KC_V,         KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JL_RSFT, KC_UP,           KC_PGDN,
-    JL_STACK,       JL_WARP, MO(3),      KC_LCTL,  KC_LALT,  KC_LGUI,    JL_LSPC,       JL_RSPC,      KC_RGUI, KC_RCTL, MO(4),   KC_LEFT, KC_DOWN, KC_RGHT
+    KC_MPRV, KC_MNXT, QK_GESC,    KC_1,     KC_2,     KC_3,       KC_4,          KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,         KC_GRV,  KC_MPLY,
+    KC_F13,  KC_F14,  KC_TAB,     KC_Q,     KC_W,     KC_E,       KC_R,          KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,         KC_DEL,
+    KC_F15,  KC_MCTL, JL_CTL_ESC, KC_A,     KC_S,     KC_D,       KC_F,          KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,          KC_PGUP,
+    KC_F7,   KC_F8,   KC_LSFT,    KC_NUBS,  KC_Z,     KC_X,       KC_C,          KC_V,         KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JL_RSFT, KC_UP,           KC_PGDN,
+    KC_F18,  JL_WARP, MO(3),      KC_LCTL,  KC_LALT,  KC_LGUI,    JL_LSPC,       JL_RSPC,      KC_RGUI, KC_RCTL, MO(4),   KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [1] = LAYOUT_all(
-    KC_F1,          KC_F2,   KC_GRAVE,   KC_1,     KC_2,     KC_3,       KC_4,          KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,         KC_GRV,  KC_MPLY,
-    KC_F3,          KC_F4,   KC_TAB,     KC_Q,     KC_W,     KC_E,       KC_R,          KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,         KC_DEL,
-    KC_F5,          KC_F6,   JL_CTL_ESC, KC_A,     KC_S,     KC_D,       KC_F,          KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,          KC_PGUP,
-    KC_F7,          KC_F8,   KC_LSFT,    KC_NUBS,  KC_Z,     KC_X,       KC_C,          KC_V,         KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JL_RSFT, KC_UP,           KC_PGDN,
-    KC_F9,          KC_F10,  MO(2),      KC_LCTL,  KC_LALT,  KC_LALT,    KC_SPC,        KC_SPC,       KC_RGUI, KC_RCTL, MO(4),   KC_LEFT, KC_DOWN, KC_RGHT
+    KC_F1,   KC_F2,   KC_GRAVE,   KC_1,     KC_2,     KC_3,       KC_4,          KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,         KC_GRV,  KC_MPLY,
+    KC_F3,   KC_F4,   KC_TAB,     KC_Q,     KC_W,     KC_E,       KC_R,          KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,         KC_DEL,
+    KC_F5,   KC_F6,   JL_CTL_ESC, KC_A,     KC_S,     KC_D,       KC_F,          KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,          KC_PGUP,
+    KC_F7,   KC_F8,   KC_LSFT,    KC_NUBS,  KC_Z,     KC_X,       KC_C,          KC_V,         KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JL_RSFT, KC_UP,           KC_PGDN,
+    KC_F9,   KC_F10,  MO(2),      KC_LCTL,  KC_LALT,  KC_LALT,    KC_SPC,        KC_SPC,       KC_RGUI, KC_RCTL, MO(4),   KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [2] = LAYOUT_all(
-    KC_F11,         KC_F12,  KC_GRV,     KC_F1,    KC_F2,    KC_F3,      KC_F4,         KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,          KC_DEL,  KC_MPLY,
-    KC_F13,         KC_F14,  JL_ALT_TAB, JL_ALT_Q, MS_WHLU,  KC_END,     KC_PGUP,       KC_PAUS,      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_UP,   MS_UP,   KC_PSCR, KC_DEL,          MS_BTN3,
-    KC_F15,         KC_F16,  KC_TRNS,    MS_WHLL,  MS_WHLD,  MS_WHLR,    KC_PGDN,       KC_HOME,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, MS_LEFT, MS_RGHT, KC_TRNS, MS_BTN1,         MS_BTN2,
-    KC_F17,         KC_F18,  KC_TRNS,    KC_TRNS,  JL_SUP_Z, JL_CUT_MAC, JL_COPY_MAC,   JL_PASTE_MAC, KC_PGUP, KC_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, MS_DOWN, KC_TRNS, KC_TRNS,         KC_TRNS,
-    KC_F19,         KC_F20,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,       KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_F11,  KC_F12,  KC_GRV,     KC_F1,    KC_F2,    KC_F3,      KC_F4,         KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,          KC_DEL,  KC_MPLY,
+    KC_F13,  KC_F14,  JL_ALT_TAB, JL_ALT_Q, MS_WHLU,  KC_END,     KC_PGUP,       KC_PAUS,      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_UP,   MS_UP,   KC_PSCR, KC_DEL,          MS_BTN3,
+    KC_F15,  KC_F16,  KC_TRNS,    MS_WHLL,  MS_WHLD,  MS_WHLR,    KC_PGDN,       KC_HOME,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, MS_LEFT, MS_RGHT, KC_TRNS, MS_BTN1,         MS_BTN2,
+    KC_F17,  KC_F18,  KC_TRNS,    KC_TRNS,  JL_SUP_Z, JL_CUT_MAC, JL_COPY_MAC,   JL_PASTE_MAC, KC_PGUP, KC_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, MS_DOWN, KC_TRNS, KC_TRNS,         KC_TRNS,
+    KC_F19,  KC_F20,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,       KC_F18,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
   [3] = LAYOUT_all(
-    KC_F21,         KC_F22,  KC_LPAD,    KC_BRID,  KC_BRIU,  KC_MCTL,    KC_WWW_SEARCH, KC_TRNS,      KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS,         KC_TRNS, KC_MPLY,
-    KC_F23,         KC_F24,  KC_MCTL,    MS_BTN1,  MS_UP,    MS_BTN2,    MS_WHLU,       KC_TRNS,      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_UP,   MS_UP,   KC_TRNS, KC_BSLS,         KC_TRNS,
-   KC_TRNS,         KC_TRNS, KC_CAPS,    MS_LEFT,  MS_DOWN,  MS_RGHT,    MS_WHLD,       KC_TRNS,      MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, MS_LEFT, MS_RGHT, KC_TRNS, KC_PENT,         KC_TRNS,
-    KC_TRNS,        KC_TRNS, KC_MPLY,    KC_TRNS,  KC_MPRV,  KC_MNXT,    KC_VOLD,       KC_VOLU,      KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MS_DOWN, KC_TRNS, KC_TRNS,         KC_TRNS,
-    KC_TRNS,        KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    MS_BTN1,       KC_CAPS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_F21,  KC_F22,  KC_LPAD,    KC_BRID,  KC_BRIU,  KC_MCTL,    KC_WWW_SEARCH, KC_TRNS,      KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS,         KC_TRNS, KC_MPLY,
+    KC_F23,  KC_F24,  KC_MCTL,    MS_BTN1,  MS_UP,    MS_BTN2,    MS_WHLU,       KC_TRNS,      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_UP,   MS_UP,   KC_TRNS, KC_BSLS,         KC_TRNS,
+   KC_TRNS,  KC_TRNS, KC_CAPS,    MS_LEFT,  MS_DOWN,  MS_RGHT,    MS_WHLD,       KC_TRNS,      MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, MS_LEFT, MS_RGHT, KC_TRNS, KC_PENT,         KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_MPLY,    KC_TRNS,  KC_MPRV,  KC_MNXT,    KC_VOLD,       KC_VOLU,      KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MS_DOWN, KC_TRNS, KC_TRNS,         KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_F18,        KC_CAPS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
   [4] = LAYOUT_all(
-    KC_F21,         KC_F22,  KC_PWR,     KC_BRID,  KC_BRIU,  KC_MCTL,    KC_WWW_SEARCH, KC_TRNS,      KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, QK_CLEAR_EEPROM, QK_BOOT, KC_MPLY,
-    KC_F23,         KC_F24,  UG_TOGG,    UG_PREV,  UG_NEXT,  UG_HUED,    UG_HUEU,       UG_SATD,      UG_SATU, UG_VALD, UG_VALU, UG_SPDD, UG_SPDU, MS_UP,   KC_TRNS, DF(0),           DF(1),
-    KC_TRNS,        KC_TRNS, KC_TRNS,    RGB_M_P,  RGB_M_B,  RGB_M_R,    RGB_M_SW,      RGB_M_SN,     RGB_M_K, RGB_M_X, RGB_M_G, KC_TRNS, MS_LEFT, MS_RGHT, KC_TRNS, KC_PENT,         KC_TRNS,
-    KC_TRNS,        KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,       KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MS_DOWN, KC_TRNS, KC_TRNS,         KC_TRNS,
-    KC_TRNS,        KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_CAPS,       KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_F21,  KC_F22,  KC_PWR,     KC_BRID,  KC_BRIU,  KC_MCTL,    KC_WWW_SEARCH, KC_TRNS,      KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, QK_CLEAR_EEPROM, QK_BOOT, KC_MPLY,
+    KC_F23,  KC_F24,  UG_TOGG,    UG_PREV,  UG_NEXT,  UG_HUED,    UG_HUEU,       UG_SATD,      UG_SATU, UG_VALD, UG_VALU, UG_SPDD, UG_SPDU, MS_UP,   KC_TRNS, DF(0),           DF(1),
+    KC_TRNS, KC_TRNS, KC_TRNS,    RGB_M_P,  RGB_M_B,  RGB_M_R,    RGB_M_SW,      RGB_M_SN,     RGB_M_K, RGB_M_X, RGB_M_G, KC_TRNS, MS_LEFT, MS_RGHT, KC_TRNS, KC_PENT,         KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,       KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MS_DOWN, KC_TRNS, KC_TRNS,         KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_CAPS,       KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 };
 
@@ -104,7 +104,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
             // prefer tap: want space more than rgui; rgui+? is harder to
             // trigger, need to hold longer. E.g. rgui needs to be held longer,
             // otherwise space will be emitted
-            return PREFER_TAP;
+            return PREFER_HOLD;
         case JL_RSFT:
             return PREFER_HOLD;
         case JL_LSPC:
@@ -173,8 +173,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case JL_LSPC:
-        case JL_RSPC:
             return true;
+        case JL_RSPC:
+            return false;
         case JL_SFT_ESC:
         case JL_CTL_ESC:
             return false;
