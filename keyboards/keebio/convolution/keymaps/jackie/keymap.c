50cde7074b2b86845f8fb170943f278c8f453c79
Jackie Li <jackie.space@gmail.com>
2026-06-04 17:13:58 +0100

    JL_GESC

--- keyboards/keebio/convolution/keymaps/jackie/keymap.c
+++ keyboards/keebio/convolution/keymaps/jackie/keymap.c
@@ -28,6 +28,10 @@ enum custom_keycodes {
     JL_ALT_TAB,
     // ALT+q
     JL_ALT_Q,
+    // grave-escape that triggers on ANY held mod (not just shift/gui):
+    // esc when tapped alone, grave (mod passed through) when any mod held,
+    // so shift -> ~, gui -> cmd+`, ralt -> ralt+`, etc.
+    JL_GESC,
 };
 
 #define JL_WARP (QK_LALT | QK_LGUI | QK_LSFT)
@@ -44,7 +48,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_all(
-    KC_MPRV, KC_MNXT, KC_GRAVE,    KC_1,     KC_2,     KC_3,       KC_4,          KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,         KC_GRV,  KC_MPLY,
+    KC_MPRV, KC_MNXT, JL_GESC,     KC_1,     KC_2,     KC_3,       KC_4,          KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,         KC_GRV,  KC_MPLY,
     KC_F13,  KC_F14,  KC_TAB,     KC_Q,     KC_W,     KC_E,       KC_R,          KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,         KC_DEL,
     KC_MCTL,  KC_F15, JL_CTL_ESC, KC_A,     KC_S,     KC_D,       KC_F,          KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,          KC_PGUP,
     KC_F7,   KC_F8,   KC_LSFT,    KC_NUBS,  KC_Z,     KC_X,       KC_C,          KC_V,         KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, JL_RSFT, KC_UP,           KC_PGDN,
@@ -116,6 +120,19 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // grave-escape on any held mod: esc when tapped alone, grave otherwise.
+    // mods are passed through, so shift -> ~, gui -> cmd+`, etc.
+    if (keycode == JL_GESC) {
+        static bool sent_grave = false;
+        if (record->event.pressed) {
+            sent_grave = (get_mods() | get_weak_mods()) != 0;
+            register_code(sent_grave ? KC_GRV : KC_ESC);
+        } else {
+            unregister_code(sent_grave ? KC_GRV : KC_ESC);
+        }
+        return false;
+    }
+
     if (record->event.pressed) {
         switch (keycode) {
             case JL_SUP_Z:
